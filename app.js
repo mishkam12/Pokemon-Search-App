33e41799bb1bd6be9c338bb7b9785870e900e174
Mishka Moodley <mishkammoodley@outlook.com>
2024-07-13 16:58:34 -0400

    Restyle and added animation

--- app.js
+++ app.js
@@ -1,55 +1,84 @@
 // DOM element references
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
+const pokemonCard = document.getElementById("pokemon-card");
+const output = document.getElementById("pokemon-info");
 const pokemonName = document.getElementById("pokemon-name");
 const pokemonId = document.getElementById("pokemon-id");
- const pokemonImage = document.getElementById("sprite");
+const pokemonImage = document.getElementById("sprite");
 const weight = document.getElementById("weight");
 const height = document.getElementById("height");
 const types = document.getElementById("types");
-const hp = document.getElementById("hp");
+const table =document.getElementById("stats-table");
+const hp = document.getElementById("pokemon-hp");
 const attack = document.getElementById("attack");
 const defense = document.getElementById("defense");
 const specialAttack = document.getElementById("special-attack");
 const specialDefense = document.getElementById("special-defense");
 const speed = document.getElementById("speed");
-const output = document.getElementById("output");
 
-// to fetch and display Pokemon data
+//Type name and colors mapping
+const typeNameAndColors = {
+  bug: "#94BC4A",
+  dark: "#736C75",
+  dragon: "#6A7BAF",
+  electric: "#E59506",
+  fairy: "#E397D1",
+  fighting: "#E391D1", 
+  fire: "#EA7A3C", 
+  flying: "#7DA6DE", 
+  ghost: "#846AB6", 
+  grass: "#71C558", 
+  ground: "#CC9F4F", 
+  ice: "#70CBD4", 
+  normal: "#AAB09F", 
+  poison: "#B468B7", 
+  psychic: "#E5609B", 
+  rock: "#B2A061", 
+  steel: "#89A1B0",
+  water: "#539AE2"
+};
+
+// Fetch  and display Pokemon data
 const pokemonSearch= async()=> {
-  
   try {
     const nameOrId= searchInput.value.toLowerCase();
 
-    //update UI with Pokemon details
+    //Update UI with Pokemon details
     const response = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${nameOrId}`);
     const data = await response.json();
     const image = data.sprites.front_default;
     pokemonImage.src = image;
     pokemonImage.style.display = "inline";
-   
+    const feet = Math.floor((data.height * 3.93700787)/12);
+    const inches = Math.floor((data.height * 3.93700787)%12);
+
     pokemonName.textContent = `${data.name.toUpperCase()}`;
-    pokemonId.textContent = `#${data.id}`;
-    weight.textContent =`Weight: ${data.weight}`;
-    height.textContent = `Height: ${data.height}`;
+    pokemonId.textContent = `NO. ${data.id}`;
+    weight.textContent =`WT: ${((data.weight * 0.22046226).toFixed(1))} lbs`;
+    height.textContent = `HT: ${feet}' ${inches}"`;
     hp.textContent = `${data.stats[0].base_stat}`;
     attack.textContent = `${data.stats[1].base_stat}`;
     defense.textContent = `${data.stats[2].base_stat}`;
     specialAttack.textContent = `${data.stats[3].base_stat}`;
     specialDefense.textContent = `${data.stats[4].base_stat}`;
     speed.textContent = ` ${data.stats[5].base_stat}`;
-    types.innerHTML = data.types.map(obj=>`<span class="type ${obj.type.name}">${obj.type.name}</span>`).join(" ");
-  }
-  //handle errors
-  catch(error){
+
+    //Update types with background colors
+    types.innerHTML = data.types.map(obj=> {
+      const typeName = obj.type.name;
+      const color = typeNameAndColors[typeName];
+      return `<span class="type" style="background-color: ${color};">${typeName}</span>`}).join(" ");
+
+  //Handle errors
+} catch(error){
     alert("Pokémon not found");
     console.log("Pokémon not found");
   }  
 };
 
-//reset Pokemon details and image
+//Reset Pokemon details and image
 const reset = () => {
-  if(output) output.remove;
   pokemonName.textContent = "";
   pokemonId.textContent = "";
   weight.textContent ="";
@@ -63,8 +92,37 @@ const reset = () => {
   types.textContent = "";
 };
 
-// Event listener for search button
+//Display Pokemon details with animations
+const displayInfo = () => {
+  pokemonCard.animate([
+    {opacity: 1, transform: "translateX(0)"},
+     {opacity: 0, transform:"translateX(-100%)"}
+  ],{
+    duration: 150,
+     fill: "forwards"
+    });
+
+    setTimeout(() => {
+      pokemonCard.style.display = "none";
+
+      output.animate([
+      {opacity: 0, transform:"translateX(100%)"},
+       {opacity: 1, transform:"translateX(0)"}
+      ],{
+          duration: 150,
+          fill: "forwards"
+         });
+         output.style.display = "block";
+         table.style.display ="block";
+      }, 50);
+
+    }
+
+// Event listener for button
 searchButton.addEventListener("click", event => {
   event.preventDefault();
+  reset();
   pokemonSearch();
+  displayInfo();
+ 
   });
